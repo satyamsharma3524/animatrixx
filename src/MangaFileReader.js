5303822ac260e762933109e020df69494144cd11
Satyam Sharma <satyamsharma3524@gmail.com>
2024-04-22 00:52:13 +0530

    Added Paginated image handling in anime chapter

--- src/MangaFileReader.js
+++ src/MangaFileReader.js
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import FlatList from 'flatlist-react';
 
 
 const MangaReader = () => {
     const { pk } = useParams();
     const [images, setImages] = useState([]);
+    const [apidata, setApidata] = useState({});
+
+    const loadMoreData = async () => {
+        const response = await axios.get(apidata.next);
+        setApidata(response.data);
+        setImages([...images, ...response.data.results]);
+    }
+
+    const renderImage = (image, idx) => {
+        return (
+            <img className='px-5 mx-5' key={idx} src={`data:image/png;base64,${image}`} alt={`Page ${idx + 1}`} />
+        )
+    }
 
     useEffect(() => {
         const fetchImages = async () => {
             console.log("pk", pk)
             const response = await axios.get(`http://localhost:8000/api/chapter_images/?pk=${pk}`);
-            setImages(response.data.images);
+            setApidata(response.data);
+            setImages(response.data.results);
         };
 
         fetchImages();
@@ -19,11 +34,16 @@ const MangaReader = () => {
 
     return (
         <div className='d-flex justify-content-center flex-column px-5 mx-5 bg-dark'>
-            {images.map((image, index) => {
-                return (
-                    <img className='px-5 mx-5' key={index} src={`data:image/png;base64,${image}`} alt={`Page ${index + 1}`} />
-                )
-            })}
+            <FlatList
+                list={images}
+                renderItem={renderImage}
+                renderWhenEmpty={() => <div>List is empty!</div>}
+                loadMoreData={loadMoreData}
+                pagination={{
+                    hasMore: apidata.next !== null,
+                    loadMore: loadMoreData,
+                  }}
+            />
         </div>
     );
 };
